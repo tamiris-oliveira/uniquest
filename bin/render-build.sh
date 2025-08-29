18f296ecd6b469b840fb23c0e5630ac4cd4ef0e2
Tamiris Oliveira <btamiris73@gmail.com>
2025-08-29 15:56:11 -0300

    Add database setup to render build script
    - Add db:create, db:migrate and db:seed to build process
    - This ensures database is properly initialized on first deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:create
 bundle exec rails db:migrate
+bundle exec rails db:seed
